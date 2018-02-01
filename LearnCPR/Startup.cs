3a4e8adc7f433570afc934ce9dd4cc5668cc720e
Bronze Brown <dhamon.nightstar@gmail.com>
2018-02-01 15:29:02 -0500

    Initial commit

---
+++ LearnCPR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnCPR.Startup))]
+namespace LearnCPR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
